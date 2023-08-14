89fbb472ef3b5bc2b02048e0bbab4432612b931e
Kanagat01 <skanagat101@gmail.com>
2023-08-15 00:14:21 +0600

    added telegram-web-app script and style.css

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from config import TOKEN
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -9,9 +10,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def command_start_handler(message: types.Message):
-    kb = types.InlineKeyboardMarkup(types.InlineKeyboardButton(
-        "Открыть приложение", web_app=WebAppInfo(url="")))
+    kb = types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(
+        "Открыть приложение", web_app=WebAppInfo(url="https://kanagat01.github.io/tg-bot-with-webapp/")))
     await message.answer(f"Привет, это тестовый бот магазина кросовок!", reply_markup=kb)
 
-
-executor.start_polling(dp)
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    executor.start_polling(dp)
